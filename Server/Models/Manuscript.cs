a95b07f3050547df3df42bec8a574ea6f1cdb34d
dannynoordamdev <dannynoordam.dev@gmail.com>
2025-04-14 13:01:36 +0200

    finalized setup files -- potential models

---
+++ Server/Models/Manuscript.cs
@@ -0,0 +1,7 @@
+public class Manuscript{
+    // Wat willen wij in onze db opslaan van de gevonden manuscripten?
+
+    // id, titel, PPN nummer, aantal keer bekeken.
+
+    // zo kunnen we berekenen hoe populair een bepaald manuscript is.
+}
---
+++ Server/Models/User.cs
@@ -0,0 +1,5 @@
+public class User {
+
+    // EXP points? geen de gebruiker een bepaald level gebaseerd op bekeken manuscripten?
+    // onervaren, geintereseerde, expert etc..
+}
